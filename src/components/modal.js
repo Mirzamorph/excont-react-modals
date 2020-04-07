d49d88393ab739c5e2b6295f6eacc9336114738f
Rustam <mirzamorf@yandex.ru>
2020-04-07 14:59:45 +0300

    added first modal

---
+++ src/components/modal.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import closeIcon from '../assets/img/close.svg';
+
+const Modal = ({ children, button }) => {
+  let btnClass = 'btn';
+  let actionsClass = 'modal__actions';
+  if (button) {
+    btnClass += ' btn-outline';
+    actionsClass += ' modal__actions-space';
+  }
+  return (
+    <div className="modal modal-simple">
+      <h2 className="modal__title">Сообщение: указан неверный E-mail</h2>
+      <button type="button" className="modal__btn-close">
+        <img src={closeIcon} alt="close modal" />
+      </button>
+      <hr className="modal__line" />
+      {children}
+      <div className={actionsClass}>
+        <button type="button" className={btnClass}>
+          Закрыть
+        </button>
+        {button}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
