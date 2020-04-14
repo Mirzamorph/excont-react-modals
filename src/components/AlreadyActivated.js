d74aac8c5a2695358d0489fcf38da3b7ee834523
Rustam <mirzamorf@yandex.ru>
2020-04-14 18:56:40 +0300

    refactor components

--- src/components/AlreadyActivated.js
+++ src/components/AlreadyActivated.js
@@ -3,21 +3,19 @@ import Popup from './partials/Popup';
 
 function AlreadyActivated() {
   return (
-    <div className="container">
-      <Popup
-        title="Сообщение: учетная запись уже активирована"
-        button={
-          <button type="button" className="btn">
-            Войти
-          </button>
-        }
-      >
-        <p>
-          Учетная запись уже активирована. Для входа в систему подключите УКЭП и
-          нажмите кнопку «Войти», для отмены — «Закрыть».
-        </p>
-      </Popup>
-    </div>
+    <Popup
+      title="Сообщение: учетная запись уже активирована"
+      button={
+        <button type="button" className="btn">
+          Войти
+        </button>
+      }
+    >
+      <p>
+        Учетная запись уже активирована. Для входа в систему подключите УКЭП и
+        нажмите кнопку «Войти», для отмены — «Закрыть».
+      </p>
+    </Popup>
   );
 }
 
